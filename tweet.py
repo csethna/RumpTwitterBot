4161e1ecc8144f971803b2fd610a32f722500e55
Ryan Koch <ryan@LFQAD5316.local>
2017-06-15 14:47:57 -0500

    temporarily reverting text change

--- tweet.py
+++ tweet.py
@@ -12,7 +12,6 @@ def send(tweets):
 
 	for index, tweet in enumerate(tweets):
 		try:
-			log.log(' '.join(('Tweeting with image...', tweet['text'])))
-			api.update_with_media(images[index], tweet['text'])
+			api.update_with_media(images[index], "#MAGA #AmericaFirst #FakeNews @realDonaldTrump")
 		except tweepy.TweepError as e:
 			log.log(e)
